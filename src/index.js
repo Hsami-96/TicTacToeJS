061e8360966b82a6a86d4c9bfb20fcaf353f6ce0
Hani <hani@Hanis-MacBook-Pro.local>
2021-07-25 02:36:35 +0100

    Commit to set up project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+
+class Square extends React.Component {
+
+}
+
+class Board extends React.Component {
+
+}
+
+class Game extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Testing change v2</h1>
+            </div>
+        );
+    };
+}
+
+// ===========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game/>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
